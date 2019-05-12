3d2821b844680e0aac00aaeb86b5d3f964b100b9
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2019-05-12 14:26:52 +0400

    publish batch

---
+++ publish.bat
@@ -0,0 +1,3 @@
+dotnet publish --output ./../../bin/win10-x64 --self-contained -r win10-x64 ./src/Repositorch.Cmd
+dotnet publish --output ./../../bin/linux-x64 --self-contained -r linux-x64 ./src/Repositorch.Cmd
+dotnet publish --output ./../../bin/osx-x64 --self-contained -r osx-x64 ./src/Repositorch.Cmd
