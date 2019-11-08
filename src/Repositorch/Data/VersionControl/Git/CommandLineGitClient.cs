de68822ebadfbffa5d0c0a4c8b13e96b5ac54185
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2019-11-08 16:45:59 +0400

    VCS data refactoring

--- src/Repositorch/Data/VersionControl/Git/CommandLineGitClient.cs
+++ src/Repositorch/Data/VersionControl/Git/CommandLineGitClient.cs
@@ -3,36 +3,38 @@ using System.IO;
 
 namespace Repositorch.Data.VersionControl.Git
 {
-	public class CommandLineGitClient : CommandLineTool, IGitClient
+	public class CommandLineGitClient : CommandLineTool, IVcsData
 	{
+		private GitRevisions revisions;
+
 		public CommandLineGitClient(string repositoryPath)
 		{
 			RepositoryPath = repositoryPath;
 			GitCommand = "git";
 			Branch = "master";
-		}
-		public Stream RevList()
-		{
-			return RunCommand(
-				"rev-list {0} --topo-order --reverse --parents",
-				Branch
-			);
-		}
-		public Stream Log(string revision)
-		{
-			return RunCommand(
-				"log -n 1 -C -m --format=format:%H%n%cn%n%ce%n%ci%n%s --name-status {0}",
-				revision
-			);
-		}
-		public Stream Blame(string revision, string filePath)
-		{
-			return RunCommand(
-				"blame -l -s --root --incremental {0} -- {1}",
-				revision, ToGitPath(filePath)
-			);
+
+			revisions = new GitRevisions(GetRevList());
 		}
 
+		public string GetRevisionByNumber(int number)
+		{
+			return revisions.GetRevisionByNumber(number);
+		}
+		public Log Log(string revision)
+		{
+			using (var log = GetLog(revision))
+			{
+				var revisionNode = revisions.GetRevisionNode(revision);
+				return new GitLog(log, revisionNode.Parents, revisionNode.Children);
+			}
+		}
+		public IBlame Blame(string revision, string filePath)
+		{
+			using (var blame = GetBlame(revision, filePath))
+			{
+				return GitBlame.Parse(blame);
+			}
+		}
 		public string RepositoryPath
 		{
 			get; private set;
@@ -44,6 +46,28 @@ namespace Repositorch.Data.VersionControl.Git
 		public string Branch
 		{
 			get; set;
+		}
+
+		private Stream GetRevList()
+		{
+			return RunCommand(
+				"rev-list {0} --topo-order --reverse --parents",
+				Branch
+			);
+		}
+		private Stream GetLog(string revision)
+		{
+			return RunCommand(
+				"log -n 1 -C -m --format=format:%H%n%cn%n%ce%n%ci%n%s --name-status {0}",
+				revision
+			);
+		}
+		private Stream GetBlame(string revision, string filePath)
+		{
+			return RunCommand(
+				"blame -l -s --root --incremental {0} -- {1}",
+				revision, ToGitPath(filePath)
+			);
 		}
 		private Stream RunCommand(string cmd, params object[] objects)
 		{
