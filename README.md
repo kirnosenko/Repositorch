2b1150d5fc81fc4b89fc4b74d74de72d4ac1daa0
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-07-26 16:59:42 +0400

    some preparations for initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+Repositorch is a VCS repository analysis engine written in C#.
+It allows to gain some statistics from repository history.
+Only Git is supported at the moment.
--- src/Repositorch.Web/ClientApp/src/components/Footer.js
+++ src/Repositorch.Web/ClientApp/src/components/Footer.js
@@ -1,28 +1,28 @@
 ﻿import React, { Fragment } from 'react';
 
 var underfooter = {
-    height: "30px",
-    width: "100%"
+	height: "30px",
+	width: "100%"
 }
 
 var footer = {
-    fontSize: 12,
-    textAlign: "center",
-    position: "absolute",
-    left: "0",
-    bottom: "0px",
-    height: "30px",
-    width: "100%"
+	fontSize: 12,
+	textAlign: "center",
+	position: "absolute",
+	left: "0",
+	bottom: "0px",
+	height: "30px",
+	width: "100%"
 }
 
 export function Footer() {
-    return (
-        <Fragment>
-            <div style={underfooter}>
-            </div>
-            <div style={footer}>
-                Powered by <a href="https://github.com/kirnosenko/">Repositorch</a> 0.0.1 alpha
-            </div>
-        </Fragment>
-    )
+	return (
+		<Fragment>
+			<div style={underfooter}>
+			</div>
+			<div style={footer}>
+				Powered by <a href="https://github.com/kirnosenko/Repositorch">Repositorch</a> 0.1.0 alpha
+			</div>
+		</Fragment>
+	)
 }
