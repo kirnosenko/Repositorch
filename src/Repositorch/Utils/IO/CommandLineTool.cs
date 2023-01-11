ab2112fdb484266944aed54c55cf7e0675c1f44c
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2023-01-11 20:34:27 +0300

    downgrade to .net6 to prevent System.Diagnostic.Process deadlock problem on linux container

--- src/Repositorch/Utils/IO/CommandLineTool.cs
+++ src/Repositorch/Utils/IO/CommandLineTool.cs
@@ -6,38 +6,34 @@ namespace System.IO
 	{
 		protected Stream GetCommandOutput(string cmd, string arguments)
 		{
-			MemoryStream buf = new MemoryStream();
-			TextWriter writer = new StreamWriter(buf);
-
-			if (RunAndWaitForExit(cmd, arguments, line => writer.WriteLine(line)))
+			var output = RunAndWaitForExit(cmd, arguments);
+			if (output != null)
 			{
+				MemoryStream buf = new MemoryStream();
+				TextWriter writer = new StreamWriter(buf);
+				writer.Write(output);
 				writer.Flush();
 				buf.Seek(0, SeekOrigin.Begin);
+
 				return buf;
 			}
 
 			return null;
 		}
-		protected bool RunAndWaitForExit(string command, string arguments, Action<string> lineOutput)
+
+		protected string RunAndWaitForExit(string command, string arguments)
 		{
 			ProcessStartInfo psi = new ProcessStartInfo(command, arguments);
 			psi.UseShellExecute = false;
 			psi.RedirectStandardOutput = true;
-			psi.RedirectStandardError = true;
 
 			Process process = new Process();
 			process.StartInfo = psi;
-			process.OutputDataReceived += (s, e) =>
-			{
-				if (e.Data != null)
-				{
-					lineOutput(e.Data);
-				}
-			};
 			process.Start();
-			process.BeginOutputReadLine();
+			var output = process.StandardOutput.ReadToEnd();
 			process.WaitForExit();
-			return process.ExitCode == 0;
+			
+			return process.ExitCode == 0 ? output : null;
 		}
 	}
 }
